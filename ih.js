ec9a1ff35bbb187559444738b62899bb6b0023d9
Dominic McDevitt-Parks <mcdevitd@gmail.com>
2017-02-01 11:18:15 -0500

    update language

--- ih.js
+++ ih.js
@@ -12,7 +12,7 @@ jQuery(document).ready(function($){
 						images = images + '<a href="https://catalog.archives.gov/id/' + t.opaResponse.results.result[i].naId + '"><img title="Title: ' + t.opaResponse.results.result[i].briefResults.titleLine[0].value + '; Creator: ' + t.opaResponse.results.result[i].briefResults.metadataArea[2].value + '; National Archives Identifier: ' + t.opaResponse.results.result[i].briefResults.metadataArea[0].value + '" src="https://catalog.archives.gov/OpaAPI/media/' + t.opaResponse.results.result[i].naId + '/' + t.opaResponse.results.result[i].thumbnailFile + '" height="50" width="50" alt="' + t.opaResponse.results.result[i].briefResults.titleLine[0].value + '"/></a>';
 						}
 					total = t.opaResponse.results['@total']
-			$('#images').html('<p>There are currently <strong>' + total + '</strong> record with citizen-contributed images. Showing 500:</p><br/>' + images)
+			$('#images').html('<p>There are currently <strong>' + total + '</strong> records with citizen-contributed images. Showing 500:</p><br/>' + images)
 			});
 		}
 			o = 0;
