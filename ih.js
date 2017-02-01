6a3a5b6ea877920c632c627b471a2ddbd3541e9f
Dominic McDevitt-Parks <mcdevitd@gmail.com>
2017-02-01 14:36:08 -0500

    restrict location, fix a couple other issues

--- ih.js
+++ ih.js
@@ -7,12 +7,23 @@ jQuery(document).ready(function($){
 			function generate(o) {
 				$("#images").html('<center><img width=500 src="https://catalog.archives.gov/images/loader.gif"/></center>');
 				var images = '';
-				$.getJSON('https://catalog.archives.gov/OpaAPI/iapi/v1?action=search&q=%22citizen%20contributor%22&tabType=online&sort=naIdSort%20desc&rows=500&offset=' + o, function(t) {
+				$.getJSON('https://catalog.archives.gov/OpaAPI/iapi/v1?action=search&q=%22citizen%20contributor%22&tabType=online&sort=naIdSort%20desc&rows=500&f.locationIds=32 or 36&offset=' + o, function(t) {
 					for (i = 0; i < t.opaResponse.results.result.length; i++) {
-						images = images + '<a href="https://catalog.archives.gov/id/' + t.opaResponse.results.result[i].naId + '"><img title="Title: ' + t.opaResponse.results.result[i].briefResults.titleLine[0].value + '; Creator: ' + t.opaResponse.results.result[i].briefResults.metadataArea[2].value + '; National Archives Identifier: ' + t.opaResponse.results.result[i].briefResults.metadataArea[0].value + '" src="https://catalog.archives.gov/OpaAPI/media/' + t.opaResponse.results.result[i].naId + '/' + t.opaResponse.results.result[i].thumbnailFile + '" height="50" width="50" alt="' + t.opaResponse.results.result[i].briefResults.titleLine[0].value + '"/></a>';
+						for (d = 0; d < t.opaResponse.results.result[i].briefResults.metadataArea.length; d++) {
+							if (t.opaResponse.results.result[i].briefResults.metadataArea[d].name == 'creators') { creators = t.opaResponse.results.result[i].briefResults.metadataArea[d].value }
+							if (t.opaResponse.results.result[i].briefResults.metadataArea[d].name == 'naId') { naid = t.opaResponse.results.result[i].briefResults.metadataArea[d].value }
+						}
+						images = images + '<a href="https://catalog.archives.gov/id/' + t.opaResponse.results.result[i].naId + '"><img data-html="true" title="Title: ' + t.opaResponse.results.result[i].briefResults.titleLine[0].value + '; Creator: ' + creators + '; National Archives Identifier: ' + naid + '" src="https://catalog.archives.gov/OpaAPI/media/' + t.opaResponse.results.result[i].naId + '/' + t.opaResponse.results.result[i].thumbnailFile + '" height="50" width="50" /></a>';
 						}
 					total = t.opaResponse.results['@total']
-			$('#images').html('<p>There are currently <strong>' + total + '</strong> records with citizen-contributed images. Showing 500:</p><br/>' + images)
+			if ((t.opaResponse.results.result.length == 500) && ((total - o) !== 500)) {
+				$('#images').html('<p>There are currently <strong>' + total + '</strong> records with citizen-contributed images. Showing 500:</p><br/>' + images);
+				$('#more').show()
+			}
+			else {
+				$('#more').hide();
+				$('#images').html('<p>There are currently <strong>' + total + '</strong> records with citizen-contributed images. This is the end of the results set. Showing final ' + t.opaResponse.results.result.length + ':</p><br/>' + images)
+			}
 			});
 		}
 			o = 0;
