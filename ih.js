00ecc394614fc3ad3740e910a054ea036a95bdc7
Dominic McDevitt-Parks <mcdevitd@gmail.com>
2017-02-13 12:50:31 -0500

    add widget for main page

--- ih.js
+++ ih.js
@@ -7,13 +7,24 @@ jQuery(document).ready(function($){
 			function generate(o) {
 				$("#images").html('<center><img width=500 src="https://catalog.archives.gov/images/loader.gif"/></center>');
 				var images = '';
-				$.getJSON('https://catalog.archives.gov/OpaAPI/iapi/v1?action=search&q=%22citizen%20contributor%22&tabType=online&sort=naIdSort%20desc&rows=500&f.locationIds=32 or 36&offset=' + o, function(t) {
+				rows= 500;
+				if (location.href.split("/").slice(-1) == 'innovation-hub.html') { rows = 20 };
+				$.getJSON('https://catalog.archives.gov/OpaAPI/iapi/v1?action=search&q=%22citizen%20contributor%22&tabType=online&sort=naIdSort%20desc&rows=' + rows + '&f.locationIds=32 or 36&offset=' + o, function(t) {
 					for (i = 0; i < t.opaResponse.results.result.length; i++) {
 						for (d = 0; d < t.opaResponse.results.result[i].briefResults.metadataArea.length; d++) {
-							if (t.opaResponse.results.result[i].briefResults.metadataArea[d].name == 'creators') { creators = t.opaResponse.results.result[i].briefResults.metadataArea[d].value }
+							if (t.opaResponse.results.result[i].briefResults.metadataArea[d].name == 'creators') {
+								creators = t.opaResponse.results.result[i].briefResults.metadataArea[d].value
+								if (creators.isArray == 'True') {
+									creatorarray = creators
+									creators = creatorarray[0]
+// 									for (c = 0; c < creators.length; c++) {
+// 										creators = creators + '&#013;' + creatorarray[c]
+// 										}
+									}
+								}
 							if (t.opaResponse.results.result[i].briefResults.metadataArea[d].name == 'naId') { naid = t.opaResponse.results.result[i].briefResults.metadataArea[d].value }
 						}
-						images = images + '<a href="https://catalog.archives.gov/id/' + t.opaResponse.results.result[i].naId + '"><img data-html="true" title="Title: ' + t.opaResponse.results.result[i].briefResults.titleLine[0].value + '; Creator: ' + creators + '; National Archives Identifier: ' + naid + '" src="https://catalog.archives.gov/OpaAPI/media/' + t.opaResponse.results.result[i].naId + '/' + t.opaResponse.results.result[i].thumbnailFile + '" height="50" width="50" /></a>';
+						images = images + '<a href="https://catalog.archives.gov/id/' + t.opaResponse.results.result[i].naId + '"><img data-html="true" title="Title:&#013;&#34;' + t.opaResponse.results.result[i].briefResults.titleLine[0].value + '&#34;&#013;&#013;Creator:&#013;' + creators + '&#013;&#013;National Archives Identifier: ' + naid + '" src="https://catalog.archives.gov/OpaAPI/media/' + t.opaResponse.results.result[i].naId + '/' + t.opaResponse.results.result[i].thumbnailFile + '" height="50" width="50" /></a>';
 						}
 					total = t.opaResponse.results['@total']
 			if ((t.opaResponse.results.result.length == 500) && ((total - o) !== 500)) {
@@ -23,6 +34,8 @@ jQuery(document).ready(function($){
 			else {
 				$('#more').hide();
 				$('#images').html('<p>There are currently <strong>' + total + '</strong> records with citizen-contributed images. This is the end of the results set. Showing final ' + t.opaResponse.results.result.length + ':</p><br/>' + images)
+				$('#imageswidget').html('<p>There are currently <strong>' + total + '</strong> records with citizen-contributed images from the Innovation Hub. Showing 10:</p><br/>' + images);
+				
 			}
 			});
 		}
